087b81ff5a06517dcfca27a7ebbb7d51a1911a0a
Ricardo Mendes <mendes.r@gmx.com>
2023-01-14 22:22:46 +0000

    Print bit from file

--- bit-reader.c
+++ bit-reader.c
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-//char *file_path = "data/red-circle.svg";
+// char *file_path = "data/red-circle.svg";
 char *file_path = "data/hello.txt";
 long file_length;
 char *buffer;
@@ -14,15 +14,30 @@ int main()
     file_length = ftell(p_file);
     rewind(p_file);
 
-    buffer = (char *) malloc(file_length * sizeof(char));
+    buffer = (char *)malloc(file_length * sizeof(char));
 
     fread(buffer, file_length, 1, p_file);
     fclose(p_file);
 
     printf(buffer);
+
+    unsigned *bits = (unsigned *)malloc(file_length * 8 * sizeof(unsigned));
+    unsigned bit = 0;
+    long len = file_length * 8;
+
+    for (bit = 0; bit < len; ++bit)
+    {
+        bits[bit] = ((unsigned) buffer >> bit) & 1;
+    }
+
+    for( bit = len; bit --;)
+    {
+        printf("%u", bits[bit]);
+    }
+    printf("\n");
+
     free(buffer);
+    free(bits);
 
     return 0;
 }
-
-
