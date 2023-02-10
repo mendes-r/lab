5b56b064a8695a31042c4a8cc23f699dee415976
Ricardo Mendes <mendes.r@gmx.com>
2023-02-10 23:44:46 +0000

    CHANGE bit count logic

--- bit-reader/bit-reader.c
+++ bit-reader/bit-reader.c
@@ -1,13 +1,15 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <limits.h>
 
-char *file_path = "data/hello2.txt";
+char *file_path = "data/hello1.txt";
 long file_length;
 unsigned char *buffer;
 
 void get_bits(unsigned char *buffer);
-void show_memory_location(unsigned char *start, int len);
+void show_memory_location(unsigned char *start, long len);
 void show_bits(unsigned int *bits, long len);
+void count_bits(unsigned int *bits, long len);
 
 int main()
 {
@@ -29,22 +31,53 @@ int main()
     return 0;
 }
 
+void count_bits(unsigned int *bits, long len)
+{
+    unsigned int count = 0;
+    unsigned int previous = bits[len];
+    unsigned int bit;
+
+    for (bit = 1; bit < len; bit++)
+    {
+        count++;
+        if (bits[bit] != previous)
+        {
+            printf("%u", count);
+            count = 0;
+            previous = bits[bit];
+        }
+
+    }
+    printf("\n");
+}
+
 void get_bits(unsigned char *buffer)
 {
-    unsigned int *bits = (unsigned int *)malloc(file_length * 8 * sizeof(unsigned int));
-    unsigned int bit = 0;
-    long len = file_length * 8;
+    unsigned int *bits = (unsigned int *)malloc(file_length * CHAR_BIT * sizeof(unsigned int));
+    unsigned int byte = 0;
+    unsigned int byte_level = 0;
 
-    for (bit = 0; bit < len; ++bit)
+    for (byte = 0; byte < file_length; byte++)
     {
-        bits[bit] = ((unsigned int)buffer >> bit) & 1;
-    }
+        unsigned char b = buffer[byte];
 
-    show_bits(bits, len);
+        for (int bit = 0; bit < CHAR_BIT; bit++)
+        {
+            printf("%u", (b >> bit) & 1);
+            bits[byte_level + bit] = (b >> bit) & 1;
+        }
+
+        byte_level = byte_level + CHAR_BIT;
+    }
+    printf("\n");
+
+    show_bits(bits, (file_length * CHAR_BIT));
+    count_bits(bits, (file_length * CHAR_BIT));
+
     free(bits);
 }
 
-void show_memory_location(unsigned char *start, int len)
+void show_memory_location(unsigned char *start, long len)
 {
     int i;
     for (i = 0; i < len; i++)
@@ -55,7 +88,7 @@ void show_memory_location(unsigned char *start, int len)
 void show_bits(unsigned int *bits, long len)
 {
     unsigned int bit = 0;
-    for (bit = len; bit--;)
+    for (bit = 0; bit < len; bit++)
     {
         printf("%u", bits[bit]);
     }
