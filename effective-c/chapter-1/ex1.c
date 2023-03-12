f65f0a1bd4fb955166d5419d1d216d3881c030dd
Ricardo Mendes <mendes.r@gmx.com>
2023-03-12 17:59:58 +0000

    Implement ex1 from effectiv c

--- effective-c/chapter-1/ex1.c
+++ effective-c/chapter-1/ex1.c
@@ -1,15 +1,27 @@
 #include<stdlib.h>
 #include<stdio.h>
 
+unsigned int *counter_ptr;
+
+int retrieve()
+{
+  printf("Memory location: %p\n", (void *) counter_ptr);
+  return *counter_ptr;
+}
+
 void increment(void){
   static unsigned int counter = 0;
   counter++;
-  printf("%d ", counter);
+  // This is redundant because the address of counter will always be the same.
+  counter_ptr = &counter;
 }
 
 int main (void){
+  int counter;
   for (int i = 0; i < 5; i++){
     increment();
+    counter = retrieve();
+    printf("Counter value: %d\n\n", counter);
   }
   return 0;
 }
