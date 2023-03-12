f81573acd984fad53b3932c44e17a250766374a8
Ricardo Mendes <mendes.r@gmx.com>
2023-03-12 17:42:46 +0000

    Implement ex2 from effectiv c

--- effective-c/chapter-1/ex2.c
+++ effective-c/chapter-1/ex2.c
@@ -1,13 +1,20 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-typedef void fv(int), (*fv)(int);
+void f_one(void) { printf("Hello, I'm Function 1\n"); }
+void f_two(void) { printf("Hello, I'm Function 2\n"); }
+void f_three(void) { printf("Hello, I'm Function 3\n"); }
 
-int main(void){
-  int array[] ={function_one, function_two, function_three};
+int main(void)
+{
 
-  for (int i = 0; i < 3; i++){
-    array[i];
-  }
+  void (*f_ptr[3])(void) = {f_one, f_two, f_three};
 
+  int option;
+
+  printf("\nEnter function number you want");
+  printf("\nYou should not enter other than 1, 2 or 3: ");
+  scanf("%d", &option);
+
+  (*f_ptr[option - 1])();
 }
